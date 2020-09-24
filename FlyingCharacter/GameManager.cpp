7f0d22068947eadd4109806b41958ba1cfa73ef8
Benoit Toth <Benoit.tothdenima@gmail.com>
2020-09-24 15:11:55 +0200

    Exit button

--- FlyingCharacter/GameManager.cpp
+++ FlyingCharacter/GameManager.cpp
@@ -4,13 +4,13 @@
 
 GameManager::GameManager()
 {
-    dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
-    dwBufferCoord = { 0, 0 };
-    rcRegion = { 0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1 };
-    new_style = (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX );
-    hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
-    ReadConsoleOutput(hOutput, (CHAR_INFO*)buffer, dwBufferSize,
-        dwBufferCoord, &rcRegion);
+	dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
+	dwBufferCoord = { 0, 0 };
+	rcRegion = { 0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1 };
+	new_style = (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX);
+	hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
+	ReadConsoleOutput(hOutput, (CHAR_INFO*)buffer, dwBufferSize,
+		dwBufferCoord, &rcRegion);
 }
 
 void GameManager::init()
@@ -22,283 +22,315 @@ void GameManager::init()
 
 void GameManager::Input() noexcept
 {
-    //Starting the game
-    if (gameState == GAME_STATE::START || gameState == GAME_STATE::GAMEOVER)
-    {
-        if ((GetKeyState(VK_RETURN)) < 0)
-        {
-            StartGame();
-        }
-        return;
-    }
+	if ((GetKeyState(VK_ESCAPE)) < 0)
+	{
+		exit(EXIT_SUCCESS);
+	}
 
-    //Shooting
-    if ((GetKeyState(VK_SPACE)) < 0)
-    { 
-        player.Shoot();
-    }
+	//Starting the game
+	if (gameState == GAME_STATE::START || gameState == GAME_STATE::GAMEOVER)
+	{
+		if ((GetKeyState(VK_RETURN)) < 0)
+		{
+			StartGame();
+		}
+		return;
+	}
 
-    //Movement
-    Vec2<int> velocity;
+	//Shooting
+	if ((GetKeyState(VK_SPACE)) < 0)
+	{
+		player.Shoot();
+	}
 
-    if ((GetKeyState(0x51)) < 0)
-    {
-        player.facingLeft = true;
-        velocity.x = -1;
-    }
-    else if ((GetKeyState(0x44)) < 0)
-    {
-        player.facingLeft = false;
-        velocity.x = 1;
-    }
-    else
-    {
-        velocity.x = 0;
-    }
+	//Movement
+	Vec2<int> velocity;
 
-    if (GetKeyState(0x5A) < 0)
-    {
-        velocity.y = -1;
-    }
-    else if (GetKeyState(0x53) < 0)
-    {
-        velocity.y = 1;
-    }
-    else
-    {
-        velocity.y = 0;
-    }
-    player.velocity = velocity;
+	if ((GetKeyState(0x51)) < 0)
+	{
+		player.facingLeft = true;
+		velocity.x = -1;
+	}
+	else if ((GetKeyState(0x44)) < 0)
+	{
+		player.facingLeft = false;
+		velocity.x = 1;
+	}
+	else
+	{
+		velocity.x = 0;
+	}
+
+	if (GetKeyState(0x5A) < 0)
+	{
+		velocity.y = -1;
+	}
+	else if (GetKeyState(0x53) < 0)
+	{
+		velocity.y = 1;
+	}
+	else
+	{
+		velocity.y = 0;
+	}
+	player.velocity = velocity;
 }
 
 void GameManager::Update() noexcept
 {
-    if (gameState == GAME_STATE::PLAYING) //Spawn an ennemy every frame
-    {
-        SpawnEnnemy();
-        Move();
-    }
-    ChangeFrame();
+	if (gameState == GAME_STATE::PLAYING) //Spawn an ennemy every frame
+	{
+		SpawnEnnemy();
+		Move();
+	}
+	ChangeFrame();
 }
 
 void GameManager::ChangeFrame() noexcept
 {
-    if (gameState == GAME_STATE::PLAYING)
-    {
-        player.ChangeFrame();
-        for (Ennemy& ennemy : ennemies)
-        {
-            if (ennemy.IsValid())
-            {
-                ennemy.ChangeFrame();
-            }
-        }
-    }
-    if (gameState == GAME_STATE::START)
-    {
-        //change frame for start screen if there is an animation
-    }
-    if (gameState == GAME_STATE::GAMEOVER)
-    {
-        //change frame for gameover screen if there is an animation
-    }
+	if (gameState == GAME_STATE::PLAYING)
+	{
+		player.ChangeFrame();
+		for (Ennemy& ennemy : ennemies)
+		{
+			if (ennemy.IsValid())
+			{
+				ennemy.ChangeFrame();
+			}
+		}
+	}
+	if (gameState == GAME_STATE::START)
+	{
+		//change frame for start screen if there is an animation
+	}
+	if (gameState == GAME_STATE::GAMEOVER)
+	{
+		//change frame for gameover screen if there is an animation
+	}
 
 }
 
 void GameManager::StartGame() noexcept
 {
-    gameState = GAME_STATE::PLAYING;
-    Reset();
+	gameState = GAME_STATE::PLAYING;
+	Reset();
 }
 
 void GameManager::SpawnEnnemy() noexcept
 {
 
-    for (Ennemy& ennemy : ennemies)
-    {
-        if (!ennemy.IsValid())
-        {
-            ennemy.Spawn(nextEnnemy);
-            nextEnnemy = (nextEnnemy + 1) % 4;
-            return;
-        }
-    }
+	for (Ennemy& ennemy : ennemies)
+	{
+		if (!ennemy.IsValid())
+		{
+			ennemy.Spawn(nextEnnemy);
+			nextEnnemy = (nextEnnemy + 1) % 4;
+			return;
+		}
+	}
 }
 
 void GameManager::Move() noexcept
 {
-    player.Move();
+	player.Move();
 
-    for (Projectile& projectile : player.projectiles)
-    {
-        if (projectile.isValid())
-        {
-            projectile.Move();
-        }
-    }
-    for (Ennemy& ennemy : ennemies)
-    {
-        if (ennemy.IsValid())
-        {
-            ennemy.MoveTo(player.posX, player.posY);
-        }
-    }
+	for (Projectile& projectile : player.projectiles)
+	{
+		if (projectile.isValid())
+		{
+			projectile.Move();
+		}
+	}
+	for (Ennemy& ennemy : ennemies)
+	{
+		if (ennemy.IsValid())
+		{
+			ennemy.MoveTo(player.posX, player.posY);
+		}
+	}
 }
 
 void GameManager::Flush() noexcept
 {
-    for (int i = 0; i < SCREEN_WIDTH; i++)
-    {
-        for (int j = 0; j < SCREEN_HEIGHT; j++)
-        {
-            buffer[j][i].Char.AsciiChar = ' ';
-        }
-    }
+	for (int i = 0; i < SCREEN_WIDTH; i++)
+	{
+		for (int j = 0; j < SCREEN_HEIGHT; j++)
+		{
+			buffer[j][i].Char.AsciiChar = ' ';
+		}
+	}
 }
 
 void GameManager::WriteFrame() noexcept // Warning buffer is [Y][X]
 {
 
-    if (gameState == GAME_STATE::START)
-    {
-        //draw start screen
-        return;
-    }
+	if (gameState == GAME_STATE::START)
+	{
+		//draw start screen
+		char text[28] = "Welcome to pewpewland !";
+		for (int i = 0; i < 28; i++)
+		{
+			buffer[SCREEN_HEIGHT / 2 - 1][SCREEN_WIDTH / 2 - 14 + i].Char.AsciiChar = text[i];
+			buffer[SCREEN_HEIGHT / 2 - 1][SCREEN_WIDTH / 2 - 14 + i].Attributes = 0x02;
+		}
+		char text2[21] = "Press enter to start";
+		for (int i = 0; i < 21; i++)
+		{
+			buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - 14 + i].Char.AsciiChar = text2[i];
+			buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - 14 + i].Attributes = 0x02;
+		}
+		return;
+	}
 
-    if (gameState == GAME_STATE::GAMEOVER)
-    {
-        //draw game over screen
-        return;
-    }
+	if (gameState == GAME_STATE::GAMEOVER)
+	{
+		//draw game over screen
+		std::string s = "You die ! your score : ";
+		s += std::to_string(score);
+		int size = s.size();
+		for (int i = 0; i < size; i++)
+		{
+			buffer[SCREEN_HEIGHT / 2 - 1][SCREEN_WIDTH / 2 + i - size / 2].Char.AsciiChar = s[i];
+			buffer[SCREEN_HEIGHT / 2 - 1][SCREEN_WIDTH / 2 + i - size / 2].Attributes = 0x02;
+		}
+		char text2[23] = "Press enter to restart";
+		for (int i = 0; i < 23; i++)
+		{
+			buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - size / 2 + i].Char.AsciiChar = text2[i];
+			buffer[SCREEN_HEIGHT / 2][SCREEN_WIDTH / 2 - size / 2 + i].Attributes = 0x02;
+		}
+		return;
+		return;
+	}
 
-    //Projectiles
-    for (Projectile &projectile : player.projectiles)
-    {
-        if (projectile.isValid())
-        {
-            buffer[projectile.getY()][projectile.getX()].Char.AsciiChar = player.projectileFrame;
-            buffer[projectile.getY()][projectile.getX()].Attributes = player.projectileColor;
-        }
-    }
+	//Projectiles
+	for (Projectile& projectile : player.projectiles)
+	{
+		if (projectile.isValid())
+		{
+			buffer[projectile.getY()][projectile.getX()].Char.AsciiChar = player.projectileFrame;
+			buffer[projectile.getY()][projectile.getX()].Attributes = player.projectileColor;
+		}
+	}
 
-    //Ennemies
-    for (Ennemy& ennemy : ennemies)
-    {
-        if (ennemy.IsValid())
-        {
-            for (int i = 0; i < ennemy.sizeX; i++)
-            {
-                int posX = ennemy.posX + i;
-                if (posX > SCREEN_WIDTH - 1 || posX < 0) //discard every characters that is out of the screen
-                {
-                    continue;
-                }
+	//Ennemies
+	for (Ennemy& ennemy : ennemies)
+	{
+		if (ennemy.IsValid())
+		{
+			for (int i = 0; i < ennemy.sizeX; i++)
+			{
+				int posX = ennemy.posX + i;
+				if (posX > SCREEN_WIDTH - 1 || posX < 0) //discard every characters that is out of the screen
+				{
+					continue;
+				}
 
-                for (int j = 0; j < ennemy.sizeY; j++)
-                {
+				for (int j = 0; j < ennemy.sizeY; j++)
+				{
 
-                    int posY = ennemy.posY + j;
+					int posY = ennemy.posY + j;
 
-                    if (posY > SCREEN_HEIGHT - 1 || posY < 0)
-                    {
-                        continue;
-                    }
+					if (posY > SCREEN_HEIGHT - 1 || posY < 0)
+					{
+						continue;
+					}
 
-                    if (buffer[posY ][posX].Char.AsciiChar == '*') //if we redraw onto a projectile, the ennemy dies
-                    {
-                        score++;
-                        ennemy.Kill();
-                    }
-                    buffer[posY][posX].Char.AsciiChar = ennemy.anim[ennemy.frame][i + ennemy.sizeX * j];
-                    buffer[posY][posX].Attributes = ennemy.color;
-                }
-            }
-        }
-    }
+					if (buffer[posY][posX].Char.AsciiChar == '*') //if we redraw onto a projectile, the ennemy dies
+					{
+						score++;
+						ennemy.Kill();
+					}
+					buffer[posY][posX].Char.AsciiChar = ennemy.anim[ennemy.frame][i + ennemy.sizeX * j];
+					buffer[posY][posX].Attributes = ennemy.color;
+				}
+			}
+		}
+	}
 
-    //Player
+	//Player
 
-    if (player.facingLeft) //draws the player facing left
-    {
-        for (int i = 0; i < player.sizeX; i++)
-        {
-            for (int j = 0; j < player.sizeY; j++)
-            {
-                if (buffer[player.posY + j][player.posX + i].Char.AsciiChar != ' ' && buffer[player.posY + j][player.posX + i].Char.AsciiChar != '*') 
-                { //if we redraw the player onto something that is not a space or a projectile, then it's an ennemy so we die
-                    GameOver();
-                    return;
-                }
-                buffer[player.posY + j][player.posX + i].Char.AsciiChar = player.idleLeft[player.frame][i + player.sizeX * j];
-                buffer[player.posY + j][player.posX + i].Attributes = player.color;
-            }
-        }
-    }
-    else //draws the player facing right
-    {
-        for (int i = 0; i < player.sizeX; i++)
-        {
-            for (int j = 0; j < player.sizeY; j++)
-            {
-                if (buffer[player.posY + j][player.posX + i].Char.AsciiChar != ' ' && buffer[player.posY + j][player.posX + i].Char.AsciiChar != '*')
-                { //if we write the player onto something that is not a space or a projectile, then it's an ennemy so we die
-                    GameOver();
-                    return;
-                }
-                buffer[player.posY + j][player.posX + i].Char.AsciiChar = player.idleRight[player.frame][i + player.sizeX * j];
-                buffer[player.posY + j][player.posX + i].Attributes = player.color;
-            }
-        }
-    }
+	if (player.facingLeft) //draws the player facing left
+	{
+		for (int i = 0; i < player.sizeX; i++)
+		{
+			for (int j = 0; j < player.sizeY; j++)
+			{
+				if (buffer[player.posY + j][player.posX + i].Char.AsciiChar != ' ' && buffer[player.posY + j][player.posX + i].Char.AsciiChar != '*')
+				{ //if we redraw the player onto something that is not a space or a projectile, then it's an ennemy so we die
+					GameOver();
+					return;
+				}
+				buffer[player.posY + j][player.posX + i].Char.AsciiChar = player.idleLeft[player.frame][i + player.sizeX * j];
+				buffer[player.posY + j][player.posX + i].Attributes = player.color;
+			}
+		}
+	}
+	else //draws the player facing right
+	{
+		for (int i = 0; i < player.sizeX; i++)
+		{
+			for (int j = 0; j < player.sizeY; j++)
+			{
+				if (buffer[player.posY + j][player.posX + i].Char.AsciiChar != ' ' && buffer[player.posY + j][player.posX + i].Char.AsciiChar != '*')
+				{ //if we write the player onto something that is not a space or a projectile, then it's an ennemy so we die
+					GameOver();
+					return;
+				}
+				buffer[player.posY + j][player.posX + i].Char.AsciiChar = player.idleRight[player.frame][i + player.sizeX * j];
+				buffer[player.posY + j][player.posX + i].Attributes = player.color;
+			}
+		}
+	}
 
-    //Draw score
-    std::string s = "Score: ";
-    s += std::to_string(score);
-    int size = s.size();
-    for (int i = 0; i < size; i++)
-    {
-        buffer[1][SCREEN_WIDTH + i - size-2].Char.AsciiChar = s[i];
-        buffer[1][SCREEN_WIDTH + i - size-2].Attributes = 0x02;
-    }
+	//Draw score
+	std::string s = "Score: ";
+	s += std::to_string(score);
+	int size = s.size();
+	for (int i = 0; i < size; i++)
+	{
+		buffer[1][SCREEN_WIDTH + i - size - 2].Char.AsciiChar = s[i];
+		buffer[1][SCREEN_WIDTH + i - size - 2].Attributes = 0x02;
+	}
 }
 
 void GameManager::Draw()
-{ 
-    WriteConsoleOutput(hOutput, (CHAR_INFO*)buffer, dwBufferSize,
-        dwBufferCoord, &rcRegion);
+{
+	WriteConsoleOutput(hOutput, (CHAR_INFO*)buffer, dwBufferSize,
+		dwBufferCoord, &rcRegion);
 }
 
 void GameManager::GameOver() noexcept
 {
-    gameState = GAME_STATE::GAMEOVER;
+	gameState = GAME_STATE::GAMEOVER;
 }
 
 void GameManager::Reset() noexcept //Replace the player in the center of the screen, reset the score and kill every ennemies/projectiles
 {
-    player.posX = SCREEN_WIDTH / 2;
-    player.posY = SCREEN_HEIGHT / 2;
-    score = 0;
-    for (Ennemy &ennemy : ennemies)
-    {
-        ennemy.Kill();
-    }
-    for (Projectile &projectile : player.projectiles)
-    {
-        projectile.setValid(false);
-    }
+	player.posX = SCREEN_WIDTH / 2;
+	player.posY = SCREEN_HEIGHT / 2;
+	score = 0;
+	for (Ennemy& ennemy : ennemies)
+	{
+		ennemy.Kill();
+	}
+	for (Projectile& projectile : player.projectiles)
+	{
+		projectile.setValid(false);
+	}
 }
 
 LONG_PTR GameManager::setConsoleWindowStyle(INT n_index)
 {
-    /*The function does not clear the last error information. if last value was zero.*/
-    SetLastError(NO_ERROR);
+	/*The function does not clear the last error information. if last value was zero.*/
+	SetLastError(NO_ERROR);
 
-    HWND hwnd_console = GetConsoleWindow();
-    LONG_PTR style_ptr = SetWindowLongPtr(hwnd_console, n_index, new_style);
-    SetWindowPos(hwnd_console, 0, 0, 0, 0, 0, SWP_NOZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_DRAWFRAME);
+	HWND hwnd_console = GetConsoleWindow();
+	LONG_PTR style_ptr = SetWindowLongPtr(hwnd_console, n_index, new_style);
+	SetWindowPos(hwnd_console, 0, 0, 0, 0, 0, SWP_NOZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_DRAWFRAME);
 
-    //show window after updating
-    ShowWindow(hwnd_console, SW_SHOW);
+	//show window after updating
+	ShowWindow(hwnd_console, SW_SHOW);
 
-    return style_ptr;
+	return style_ptr;
 }
